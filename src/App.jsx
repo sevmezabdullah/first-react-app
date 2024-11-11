b77f1efac0fbe6e4bfde863c000c8169975d1987
dtasevmez <abdullahsevmez@gmail.com>
2024-11-11 21:07:27 +0300

    useRef ve useMemo tamamlandı

--- src/App.jsx
+++ src/App.jsx
@@ -1,90 +1,88 @@
-
-import { useEffect, useState } from 'react'
-import ContactForm from './components/ContactForm';
-import ContactList from './components/ContactList';
-import SearchBox from './components/SearchBox';
+import { useMemo } from "react";
+import { useRef, useState } from "react";
 
 
 // Rehber Uygulaması
 const App = () => {
-  //formik - formlardaki olayları dinleyip verileri kontrol eder.
+  // useRef - 
 
-  //yup - veri validasyonu yapar.
+  // deprecated - kullanımdan kaldırıldı. React 19 ile kullanımdan kaldırıldı.
+  //  Performans optimizasyonu için kullanılır. Ağır işlemleri ihtiyaç olduğunda 1 kere yapar. Ve daha sonra ön belleğe alır.
+  // Doğru kullanılmazsa daha çok performans problemi yaratabilirler.
+
+  // Bir fonksiyonumuz var. Ve bu fonksiyon ram de 20 mb kaynak tüketiyor.
+  // Her fonksiyon useMemo ve useCallback olarak tanımlanırsa bu sefer de ram de ciddi kaynak tüketebilir.
+  // useMemo ve useCallback - 
 
 
-  // Contact State
-  const [contacts, setContacts] = useState([]);
-  const [baseList, setBaseList] = useState([]);
-  const [editIndex, setEditIndex] = useState(null);
-  const [searchValue, setSearchValue] = useState('');
+  const [text, setText] = useState("");
+  const [count, setCount] = useState(0);
 
 
-  useEffect(() => {
-    filterContacts(searchValue)
+  const inputRef = useRef()
 
-    if (searchValue === '') {
-      setContacts(baseList)
+  const expensiveResult = useMemo(() => expensiveCalculation(text), [text])
+
+
+  function expensiveCalculation(text) {
+    console.log("Expensive Calculation : ", text);
+    let result = "";
+
+    // 1000000 tane islem
+    for (let i = 0; i < 1000000; i++) {
+      result = `${text} ${i}`
     }
-  }, [searchValue])
-
-
-  const filterContacts = (value) => {
-    setContacts(contacts.filter((contact) => contact.firstName.toLowerCase().includes(value.toLowerCase())))
+    return result;
   }
 
-  const handleSubmit = (values, { resetForm }) => {
-    //  console.table(values);
 
-    //Eğer güncelleme yapılırsa
-    if (editIndex !== null) {
-      const updatedContacts = [...contacts];
-      updatedContacts[editIndex] = values
-      setContacts(updatedContacts);
-      setEditIndex(null);
-    }
-    // Eğer ekleme yapılırsa
-    else {
-      setContacts([...contacts, values]);
-      setBaseList([...contacts, values]);
-    }
+  const handleChange = (e) => {
+    setText(e.target.value)
 
-    resetForm();
   }
-  const handleDelete = (index) => {
-    setContacts(contacts.filter((_, i) => i !== index));
-  }
-  const handleEdit = (index) => {
-    setEditIndex(index);
+
+
+  const focusInput = () => {
+
+    inputRef.current.focus();
   }
 
 
   return (
     <>
 
-      <div className='bg-base-100'>
-        <div>
-          <h1 style={{ fontSize: "60px" }} className='text-center '>Rehber Uygulaması</h1>
+      <div>
+
+        <h1 style={{
+          color: "red",
+          fontSize: "40px",
+          textAlign: "center"
+        }}>useMemo ve useRef Kullanımı</h1>
+
+        <input style={{
+          border: "1px solid white",
+          borderRadius: "10px",
+        }} placeholder="Herhangi birşey yazınız" className="form-control p-4 mx-auto" type="text" onChange={handleChange} value={text} ref={inputRef} />
+        <p style={{
+          color: "white",
+          fontSize: "40px",
+          textAlign: "center"
+        }}>Maliyetli işlemin sonucu : {expensiveResult}</p>
+
+
+
+
+
+        <div style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          gap: "10px"
+        }}>
+          <button onClick={() => setCount(count + 1)} className="btn btn-primary">Sayıyı Artır {count}</button>
+          <button className="btn btn-primary" onClick={focusInput} type="button">Inputa Odaklan</button>
         </div>
-
-
-        {/* initialValues %99 boş geçilir. %1 ihtimal dolu başlamasını isterler*/}
-        <ContactForm initialValues={{
-          firstName: '',
-          lastName: '',
-          phoneNumber: '',
-          adress: '',
-          email: '',
-        }} onSubmit={handleSubmit} />
-
-
-
-        <SearchBox setValue={setSearchValue} value={searchValue} />
-        <div className='mt-8'>
-          <ContactList contactList={contacts} onDelete={handleDelete} onEdit={handleEdit} />
-        </div>
-
       </div>
-
 
 
 
